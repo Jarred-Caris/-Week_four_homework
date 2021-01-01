8348a859969f10148a5b44a2de33e1a5b69f7d22
Jarred Caris <jarred.caris@gmail.com>
2021-01-01 14:48:04 +1030

    added all questions and answers to javascript

--- script.js
+++ script.js
@@ -14,7 +14,11 @@ function startQuiz() {
   currentQuestion = 0;
   questionElement.classList.remove("hide");
   answerElement.classList.remove("hide");
-  console.log("Initiate");
+  nextQuestion();
+  console.log("Initiate first question");
+}
+
+function nextQuestion() {
   nextQuestion();
 }
 
@@ -22,8 +26,8 @@ function nextQuestion() {
   showQuestion(shuffleQuestions[currentQuestion]);
 }
 
-function showQuestion(question) {
-  questionText.innertext = questionText.question;
+function showQuestion(questions) {
+  questionText.innertext = questions.question;
 }
 
 function answerSelect() {}
@@ -31,11 +35,57 @@ function answerSelect() {}
 var questions = [
   {
     question: "What does DC stand for?",
-    answerText: [
+    answerBtn: [
       { text: "Delta Comics", correct: false },
       { text: "Detective Comics", correct: true },
       { text: "Dirty Comics", correct: false },
       { text: "Driving Comics", correct: false },
+    ],
+  },
+  {
+    question: "What year did Superman make his first appearance?",
+    answerBtn: [
+      { text: "1924", correct: false },
+      { text: "1938", correct: true },
+      { text: "1945", correct: false },
+      { text: "1959", correct: false },
+    ],
+  },
+  {
+    question: "What Colour was the Hulk in his first appearance?",
+    answerBtn: [
+      { text: "Green", correct: false },
+      { text: "Red", correct: false },
+      { text: "Grey", correct: true },
+      { text: "Blue", correct: false },
+    ],
+  },
+  {
+    question: "Which Batman villain broke batmans back?",
+    answerBtn: [
+      { text: "The Joker", correct: false },
+      { text: "Two face", correct: false },
+      { text: "Deathstroke", correct: false },
+      { text: "Bane", correct: true },
+    ],
+  },
+  {
+    question: "Who is the fastest superhero?",
+    answerBtn: [
+      { text: "Quicksilver", correct: false },
+      { text: "Superman", correct: false },
+      { text: "The Flash", correct: true },
+      { text: "Iron Man", correct: false },
+    ],
+  },
+  {
+    question:
+      "How much was a near perfect copy of Action Comics #1 sold for recently?",
+    answerBtn: [
+      { text: "$365,000", correct: false },
+      { text: "$860,000", correct: false },
+      { text: "$1,900,000", correct: false },
+      { text: "$3,200,000", correct: true },
     ],
   },
 ];
